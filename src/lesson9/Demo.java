4566e7cab41d757f919002e42fc4a1602401358d
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2018-12-08 17:39:06 +0200

    lesson 9

---
+++ src/lesson9/Demo.java
@@ -0,0 +1,16 @@
+package lesson9;
+
+import lesson9.utils.Checker;
+
+public class Demo {
+    public static void main(String[] args) {
+        Company company = new Company("testt", "Ukraine");
+        System.out.println(company.getCountryFounded());
+        System.out.println(company.getName());
+
+        Checker checker = new Checker();
+        System.out.println(checker.checkCompanyName(company.getName()));
+        //System.out.println(checker.companyNamesValidatedCount);
+        //company.code
+    }
+}
---
+++ src/lesson9/utils/Checker.java
@@ -0,0 +1,16 @@
+package lesson9.utils;
+
+public class Checker {
+    int companyNamesValidatedCount = 0;
+
+    public boolean checkCompanyName(String companyName) {
+//        if (companyName == "Google" || companyName == "Amazon") {
+//
+//        }
+        if (companyNamesValidatedCount > 10) {
+            return false;
+        }
+        companyNamesValidatedCount++;
+        return companyName != "Google" && companyName != "Amazon";
+    }
+}
