eb964e1e2b68d165e06c828825e31ad79af441c6
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2018-12-14 20:22:18 +0200

    lesson 13

---
+++ src/lesson13/Demo.java
@@ -0,0 +1,52 @@
+package lesson13;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        UserRepository userRepository = new UserRepository();
+
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+
+        User user = new User(1001, "Ann", "1w21212");
+        userRepository.save(user);
+
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+        userRepository.save(user);
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+
+
+
+        //сохранение юзера
+        //сохранение того же юзера
+        //когда нет места в массиве
+        //когда сохраняем null
+
+        int n = 15;
+        while(n > 0) {
+            User users1 = new User(n, "Ann", "1w21212");
+            System.out.println(userRepository.save(users1));
+            n--;
+        }
+
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+
+        userRepository.save(null);
+
+        //test update
+        user = new User(1001, "Ann", "eretertert");
+        userRepository.update(user);
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+
+        //обновление юзера
+        //когда нет юзера на обновление
+        //когда обновляем null
+
+        user = new User(9999, "Ann", "eretertert");
+        System.out.println(userRepository.update(user));
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+
+        System.out.println(userRepository.update(null));
+        System.out.println(Arrays.deepToString(userRepository.getUsers()));
+    }
+}
