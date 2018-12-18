535efa8de6399ec08f521a5831045466357725de
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2018-12-18 20:17:10 +0200

    lesson 12. homework. fix

--- src/lesson12/UkrainianBankSystem.java
+++ src/lesson12/UkrainianBankSystem.java
@@ -14,7 +14,7 @@ public class UkrainianBankSystem implements BankSystem {
 
     @Override
     public void fund(User user, int amount) {
-        if (!checkFundingLimit(user, amount)) {
+        if (amount > user.getBank().getLimitOfFunding()) {
             return;
         }
 
@@ -25,15 +25,9 @@ public class UkrainianBankSystem implements BankSystem {
     public void transferMoney(User fromUser, User toUser, int amount) {
         //снимаем деньги с fromUser
         //пополняем toUser
-        if (!checkWithdraw(fromUser, amount)) {
-            return;
-        }
-
-        if (!checkFundingLimit(toUser, amount)) {
-            return;
-        }
-
-        if (!checkTransferCurrency(fromUser, toUser)) {
+        if (!checkWithdraw(fromUser, amount)
+            || (amount > toUser.getBank().getLimitOfFunding())
+            || (fromUser.getBank().getCurrency() != toUser.getBank().getCurrency())) {
             return;
         }
 
@@ -46,50 +40,10 @@ public class UkrainianBankSystem implements BankSystem {
         fund(user, user.getSalary());
     }
 
-    private boolean checkTransferCurrency(User fromUser, User toUser) {
-        if (fromUser.getBank().getCurrency() != toUser.getBank().getCurrency()) {
-            printTransferErrorMsg(fromUser, toUser);
-
-            return false;
-        }
-
-        return true;
-    }
-
-    private void printTransferErrorMsg(User fromUser, User toUser) {
-        System.err.println("Can't transfer currency " + fromUser.getBank().getCurrency() + " to " + toUser.getBank().getCurrency());
-    }
-
-    private boolean checkFundingLimit(User user, int amount) {
-        if (amount > user.getBank().getLimitOfFunding()) {
-            printFundingErrorMsg(amount, user);
-
-            return false;
-        }
-
-        return true;
-    }
-
-    private void printFundingErrorMsg(int amount, User user) {
-        System.err.println("Can't fund money " + amount + " to user " + user.toString());
-    }
 
     private boolean checkWithdraw(User user, int amount) {
-        return checkWithdrawLimits(user, amount, user.getBank().getLimitOfWithdrawal()) &&
-                checkWithdrawLimits(user, amount, user.getBalance());
+        return (amount + user.getBank().getCommission(amount)) <= user.getBank().getLimitOfWithdrawal()
+               && (amount + user.getBank().getCommission(amount)) <= user.getBalance();
     }
 
-    private boolean checkWithdrawLimits(User user, int amount, double limit) {
-        if ((amount + user.getBank().getCommission(amount)) > limit) {
-            printWithdrawalErrorMsg(amount, user);
-
-            return false;
-        }
-
-        return true;
-    }
-
-    private void printWithdrawalErrorMsg(int amount, User user) {
-        System.err.println("Can't withdraw money " + amount + " from user " + user.toString());
-    }
 }
