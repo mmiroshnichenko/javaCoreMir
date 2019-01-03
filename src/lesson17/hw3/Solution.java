4d6f5b232a85155924d6755c5e9a78171cc18ea8
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-01-03 15:56:18 +0200

    lesson 17. homework

--- src/lesson17/hw3/Solution.java
+++ src/lesson17/hw3/Solution.java
@@ -1,8 +1,31 @@
 package lesson17.hw3;
 
 public class Solution {
-
     public String mostCountedWord(String input) {
+        String[] words = input.split(" ");
+
+        String mostCountedWord = words[0];
+        int count = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            count = 0;
+            for (int j = 0; j < words.length; j++) {
+                if (i != j && !words[i].isEmpty() && !words[j].isEmpty() && words[i].equals(words[j])) {
+                    count++;
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                mostCountedWord = words[i];
+            }
+        }
+
+        return maxCount > 0 ? mostCountedWord : null;
+    }
+
+
+    public String mostCountedWordOld(String input) {
         String[] words = getArrayOfWords(input);
         if (words.length == 0) {
             return null;
