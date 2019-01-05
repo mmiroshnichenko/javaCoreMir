fa997804b4e022b01061c4d366732280d9a11d78
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-01-05 15:56:41 +0200

    lesson 17. homework

--- src/lesson17/hw4V2/Solution.java
+++ src/lesson17/hw4V2/Solution.java
@@ -3,7 +3,8 @@ package lesson17.hw4V2;
 public class Solution {
     public boolean validate(String address) {
 
-        address = address.replace("https://", "http://")
+        address = address.trim()
+                .replace("https://", "http://")
                 .replace("//www.", "//");
 
         String protocol = address.substring(0, 7);
