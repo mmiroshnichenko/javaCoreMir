a74fef5970d152b828cc5058db91b26af71689ac
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-01-05 17:22:38 +0200

    lesson 17. homework

--- src/lesson17/hw4V2/Solution.java
+++ src/lesson17/hw4V2/Solution.java
@@ -4,20 +4,22 @@ public class Solution {
     public boolean validate(String address) {
 
         address = address.toLowerCase()
-                .replace("https://", "http://")
                 .replace("//www.", "//");
 
-        if (address.length() < 13) {
+        if (address.length() < 12) {
             return false;
         }
-
-        String protocol = address.substring(0, 7);
-        String domain = address.substring(address.length() - 4);
-        String siteName = address.substring(7, address.length() - 4);
-        if (protocol.equals("http://") && validateSiteName(siteName)
-            && (domain.equals(".com")) || domain.equals(".org") || domain.equals(".net")) {
-            return true;
+        String[] protocols = {"https://", "http://"};
+        for (String availableProtocol : protocols) {
+            String protocol = address.substring(0, availableProtocol.length());
+            String domain = address.substring(address.length() - 4);
+            String siteName = address.substring(availableProtocol.length(), address.length() - 4);
+            if (availableProtocol.equals(protocol) && validateSiteName(siteName)
+                    && (domain.equals(".com")) || domain.equals(".org") || domain.equals(".net")) {
+                return true;
+            }
         }
+
 
         return false;
     }
