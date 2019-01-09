5572e0522fd704312079b07f8d93f988296e937e
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-01-09 21:07:15 +0200

    lesson 19. homework

--- src/lesson19/home/Controller.java
+++ src/lesson19/home/Controller.java
@@ -1,17 +1,13 @@
 package lesson19.home;
 
 public class Controller {
-    public File put(Storage storage, File file) throws Exception{
+    public File put(Storage storage, File file) throws Exception {
         storage.validateNewFile(file);
         return storage.put(file);
     }
 
-    public void delete(Storage storage, File file) {
-        try {
-            storage.delete(file);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public void delete(Storage storage, File file) throws Exception {
+        storage.delete(file);
     }
 
     public void transferAll(Storage storageFrom, Storage storageTo) {
