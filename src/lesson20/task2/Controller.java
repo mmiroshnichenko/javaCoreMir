b09f9c850dcf6b32dee7bfe95d0f1d85ff79dd7a
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-01-10 21:53:36 +0200

    lesson 20. task2

---
+++ src/lesson20/task2/Controller.java
@@ -0,0 +1,24 @@
+package lesson20.task2;
+
+import lesson20.task2.exception.InternalServerException;
+import lesson20.task2.exception.LimitExceeded;
+
+public class Controller {
+    private TransactionDAO transactionDAO = new TransactionDAO();
+
+    public Transaction save(Transaction transaction) throws LimitExceeded, InternalServerException {
+        return transactionDAO.save(transaction);
+    }
+
+    Transaction[] transactionList() {
+        return transactionDAO.transactionList();
+    }
+
+    Transaction[] transactionList(String city) {
+        return transactionDAO.transactionList(city);
+    }
+
+    Transaction[] transactionList(int amount) {
+        return transactionDAO.transactionList(amount);
+    }
+}
