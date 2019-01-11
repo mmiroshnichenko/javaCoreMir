be5cc2006190cdf4f553b8257cb9380ccb9edd36
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-01-11 22:23:38 +0200

    lesson 19. home

--- src/lesson19/home/Controller.java
+++ src/lesson19/home/Controller.java
@@ -1,25 +1,28 @@
 package lesson19.home;
 
 public class Controller {
+
     public File put(Storage storage, File file) throws Exception {
-        storage.validateNewFile(file);
-        return storage.put(file);
+        StorageDAO storageDAO = new StorageDAO(storage);
+        return storageDAO.putFile(file);
     }
 
     public void delete(Storage storage, File file) throws Exception {
-        storage.delete(file);
+        StorageDAO storageDAO = new StorageDAO(storage);
+        storageDAO.deleteFile(file);
     }
 
     public void transferAll(Storage storageFrom, Storage storageTo) throws Exception {
-        storageTo.validateNewPacketFiles(storageFrom.getFiles());
-        for (File file : storageFrom.getFiles()) {
-            storageTo.put(file);
-            storageFrom.delete(file);
-        }
+        StorageDAO storageDAOTo = new StorageDAO(storageTo);
+        StorageDAO storageDAOFrom = new StorageDAO(storageFrom);
+
+        storageDAOTo.putPacketFiles(storageFrom.getFiles());
+        storageDAOFrom.deleteStorageFiles();
     }
 
     public void transferFile(Storage storageFrom, Storage storageTo, long id)  throws Exception {
         File file = storageFrom.findFileById(id);
+
         put(storageTo, file);
         delete(storageFrom, file);
     }
--- src/lesson19/home/Storage.java
+++ src/lesson19/home/Storage.java
@@ -1,7 +1,5 @@
 package lesson19.home;
 
-import java.util.Arrays;
-
 public class Storage {
     private long id;
     private File[] files;
@@ -37,7 +35,7 @@ public class Storage {
         return storageSize;
     }
 
-    public File put(File file) throws Exception {
+    public File putFile(File file) throws Exception {
         for (int i = 0; i < files.length; i++) {
             if (files[i] == null) {
                 files[i] = file;
@@ -49,7 +47,7 @@ public class Storage {
         throw new Exception("Error: File(id: " + file.getId() + ") was not saved. Storage(id: " + id + ") is full");
     }
 
-    public void delete(File file) throws Exception {
+    public void deleteFile(File file) throws Exception {
         for (int i = 0; i < files.length; i++) {
             if (files[i] != null && files[i].equals(file)) {
                 files[i] = null;
@@ -72,62 +70,7 @@ public class Storage {
         throw new Exception("Error: File(id: " + fileId + ") was not found in Storage(id: " + id + ")");
     }
 
-    public void validateNewFile(File file) throws Exception {
-        try {
-            findFileById(file.getId());
-            throw new Exception("Error: File(id: " + file.getId() + ") already exists in Storage(id: " + id + ")");
-        } catch (Exception e) {
-            //System.out.println("File(id: " + file.getId() + "not found in Storage(id: " + id + "). Will be saved");
-        }
-
-        if (!Arrays.asList(formatsSupported).contains(file.getFormat())) {
-            throw new Exception("Error: File(id: " + id + ") has not supported format '" + file.getFormat()
-                    + " in Storage(id: " + id + ")");
-        }
-
-        if (file.getSize() > storageSize - getUsedSpace()) {
-            throw new Exception("Error: not enough space in Storage(id: " + id + ") for File(id: " + file.getId() + ")");
-        }
-    }
-
-    public void validateNewPacketFiles(File[] newFiles) throws Exception {
-        long filesSize = 0;
-        int countNewFiles = 0;
-        for (File file : newFiles) {
-            if (file != null) {
-                try {
-                    findFileById(file.getId());
-                    throw new Exception("Error: File(id: " + file.getId() + ") already exists in Storage(id: " + id + ")");
-                } catch (Exception e) {
-                    //System.out.println("File(id: " + file.getId() + "not found in Storage(id: " + id + "). Will be saved");
-                }
-
-                if (!Arrays.asList(formatsSupported).contains(file.getFormat())) {
-                    throw new Exception("Error: File(id: " + id + ") has not supported format '" + file.getFormat()
-                            + " in Storage(id: " + id + ")");
-                }
-                filesSize += file.getSize();
-                countNewFiles++;
-            }
-        }
-
-        if (filesSize > storageSize - getUsedSpace()) {
-            throw new Exception("Error: not enough space in Storage(id: " + id + ") for Files");
-        }
-
-        int countCells = 0;
-        for (File file : files) {
-            if (file == null) {
-                countCells++;
-            }
-        }
-
-        if (countCells < countNewFiles) {
-            throw new Exception("Error: not enough free cells in Storage(id: " + id + ") for Files");
-        }
-    }
-
-    private long getUsedSpace() {
+    public long getUsedSpace() {
         long usedSize = 0;
         for (File storageFile : files) {
             if (storageFile != null) {
