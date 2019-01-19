ace98fa288cc830aeee656b35af597f3c838c952
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-01-19 15:41:30 +0200

    lesson 27. homework2

--- src/lesson27/homework2/UserRepository.java
+++ src/lesson27/homework2/UserRepository.java
@@ -6,11 +6,9 @@ public class UserRepository {
     private static ArrayList<User> users = new ArrayList<>();
 
     public static User save(User user) throws BadRequestException {
-        try {
-            findById(user.getId());
+        User existUser = findById(user.getId());
+        if (existUser != null) {
             throw new BadRequestException(user.toString() + " already exist");
-        } catch (UserNotFoundException e) {
-            //System.out.println(user.toString() + " will be saved");
         }
 
         users.add(user);
@@ -18,24 +16,36 @@ public class UserRepository {
     }
 
     public static User update(User user) throws UserNotFoundException {
-        int index = users.indexOf(findById(user.getId()));
-        users.set(index, user);
+        int index = 0;
+        for (User elUser : users) {
+            if (user.getId() == elUser.getId()) {
+                users.set(index, user);
 
-        return users.get(index);
+                return users.get(index);
+            }
+            index++;
+        }
+
+        throw new UserNotFoundException(user.toString() + " not found in repository");
     }
 
     public static void delete(long id) throws UserNotFoundException {
-        users.remove(findById(id));
+        User user = findById(id);
+        if (user == null) {
+            throw new UserNotFoundException("User with id: " + id + " not found in repository");
+        }
+
+        users.remove(user);
     }
 
-    public static User findById(long id) throws UserNotFoundException {
+    public static User findById(long id) {
         for (User user : users) {
             if (id == user.getId()){
                 return user;
             }
         }
 
-        throw new UserNotFoundException("User with id: " + id + " not found in repository");
+        return null;
     }
 
     public static ArrayList<User> getUsers() {
