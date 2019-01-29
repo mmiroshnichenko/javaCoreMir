7508655a307f68319772ec6b0b58c27934eb9f9f
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-01-29 20:51:23 +0200

    lesson 33. homework1

---
+++ src/lesson33/homework/Demo.java
@@ -0,0 +1,7 @@
+package lesson33.homework;
+
+public class Demo {
+    public static void main(String[] args) {
+        Solution.writeToFileFromConsole("C:\\Users\\miroshnichenko\\Desktop\\test.txt");
+    }
+}
